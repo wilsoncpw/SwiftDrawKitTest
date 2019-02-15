b3d7de2f13a8dbca069c566d606bbac4a0155af4
Colin Wilson <colin.p.wilson@marks-and-spencer.com>
2019-02-15 11:07:25 +0000

    Initial Commit

---
+++ SwiftDrawKitTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftDrawKitTest
+//
+//  Created by Colin Wilson on 13/02/2019.
+//  Copyright © 2019 Colin Wilson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftDrawKitTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftDrawKitTest
+//
+//  Created by Colin Wilson on 13/02/2019.
+//  Copyright © 2019 Colin Wilson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
